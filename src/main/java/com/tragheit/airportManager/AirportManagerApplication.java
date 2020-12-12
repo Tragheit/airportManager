e9eee59254fe54492c8155b834778a18130e4db0
tragheit <rrb.lewandowski@gmail.com>
2020-12-12 21:04:52 +0100

    initial commit

---
+++ src/main/java/com/tragheit/airportManager/AirportManagerApplication.java
@@ -0,0 +1,13 @@
+package com.tragheit.airportManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tragheit/airportManager/AirportManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tragheit.airportManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
